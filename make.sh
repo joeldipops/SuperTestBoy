538b7cac7738e5fac9063392cfeadfd874067a56
joeldipops <joeldipops@gmail.com>
2019-01-30 21:24:31 +1000

    A bit more config

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ rgbasm -o ../bin/superTestBoy.o ./main.asm
 cd ..
 rgblink -o ./bin/superTestBoy.gb -n ./bin/superTestBoy.sym ./bin/superTestBoy.o
 rgbfix -v -p 0 ./bin/superTestBoy.gb
-cp ./bin/superTestBoy.gb ../TransferBoy/includes
+cp ./bin/superTestBoy.gb ../TransferBoy/assets/superTestBoy.gb
 cp ./includes/addresses.asm ../gbz80-pseudoOps/addresses.inc
 cp ./includes/ops.asm ../gbz80-pseudoOps/ops.inc
 wine ~/Projects/tools/bgb/bgb64.exe ./bin/superTestBoy.gb
